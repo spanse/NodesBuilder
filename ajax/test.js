edb7fe14a16ca388f77953e18a0f2fd6964a33bc
spencer lennie <s4spencer@live.ca>
2017-01-27 19:18:07 -0600

    Completed session handling, preparing server-side AJAX handling and db connection

---
+++ ajax/test.js
@@ -0,0 +1,17 @@
+﻿var connection = require('../dbconnect');
+
+function exampleOutput() {
+    console.log("exampleOutput ran successfully.");
+}
+
+function exampleQuery() {
+    new connection.Request().query('SELECT * FROM dbo.Users').then(function (recordset) {
+        console.log(recordset);
+        console.log("exampleQuery ran successfully.");
+    });
+}
+
+module.exports = exampleOutput;
+
+//Summary:
+//connection2 DOES successfully reference variables in dbconnect.js (although intelisense doesn't seem to find them)
---
+++ dbconnect.js
@@ -0,0 +1,15 @@
+﻿var sql = require('mssql');
+
+var connection = new sql.Connection({
+    user: 'remote',
+    password: 'Marksman12',
+    server: '216.197.156.69', // You can use 'localhost\\instance' to connect to named instance
+    database: 'WebServer'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
+var request = new sql.Request();
+module.exports = connection;
+module.exports = request;
